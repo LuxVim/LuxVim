9c0a0db3a976bc179a013b165421c91954bf14aa
josstei <joseph.steinbrenner@yahoo.com>
2025-07-18 00:21:35 -0400

    formatting update

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,59 +1,59 @@
 return {
-  -- File management and fuzzy finding
-  {
-    "junegunn/fzf",
-    dependencies = { "junegunn/fzf.vim" },
-    config = function()
-      vim.g.fzf_layout = { down = '20%' }
-    end,
-  },
+    -- File management and fuzzy finding
+    {
+        "junegunn/fzf",
+        dependencies = { "junegunn/fzf.vim" },
+        config = function()
+            vim.g.fzf_layout = { down = '20%' }
+        end,
+    },
 
-  -- File explorer
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup({
-        view = {
-          width = 30,
-          side = "left",
-        },
-        renderer = {
-          group_empty = true,
-          icons = {
-            glyphs = {
-              default = "",
-              symlink = "",
-              folder = {
-                arrow_closed = "",
-                arrow_open = "",
-                default = "",
-                open = "",
-                empty = "",
-                empty_open = "",
-                symlink = "",
-                symlink_open = "",
-              },
-            },
-          },
-        },
-        filters = {
-          dotfiles = false,
-        },
-        git = {
-          enable = true,
-        },
-        actions = {
-          open_file = {
-            quit_on_open = false,
-          },
-        },
-      })
-    end,
-  },
+    -- File explorer
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("nvim-tree").setup({
+                view = {
+                    width = 30,
+                    side = "left",
+                },
+                renderer = {
+                    group_empty = true,
+                    icons = {
+                        glyphs = {
+                            default = "",
+                            symlink = "",
+                            folder = {
+                                arrow_closed = "",
+                                arrow_open = "",
+                                default = "",
+                                open = "",
+                                empty = "",
+                                empty_open = "",
+                                symlink = "",
+                                symlink_open = "",
+                            },
+                        },
+                    },
+                },
+                filters = {
+                    dotfiles = false,
+                },
+                git = {
+                    enable = true,
+                },
+                actions = {
+                    open_file = {
+                        quit_on_open = false,
+                    },
+                },
+            })
+        end,
+    },
 
-  -- Smooth scrolling
-  {
-    "psliwka/vim-smoothie",
-  },
+    -- Smooth scrolling
+    {
+        "psliwka/vim-smoothie",
+    },
 }
