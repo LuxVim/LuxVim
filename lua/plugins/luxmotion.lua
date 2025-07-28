17fc0ef1df18de70805d31a89c17e438cba1f08b
josstei <joseph.steinbrenner@yahoo.com>
2025-07-27 23:47:17 -0400

    update to luxmotion to leverage new scrolling features

--- lua/plugins/luxmotion.lua
+++ lua/plugins/luxmotion.lua
@@ -3,19 +3,19 @@ return {
     config = function()
         require("luxmotion").setup({
             cursor = {
-                duration = 10,
+                duration = 250,
                 easing = "ease-out",
                 enabled = true,
             },
             scroll = {
-                duration = 380,
+                duration = 400,
                 easing = "ease-out", 
                 enabled = true,
             },
             keymaps = {
                 cursor = true,
                 scroll = true,
-                experimental = false,
+                experimental = true,
             },
         })
     end,
