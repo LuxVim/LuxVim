dc2b158fa3de8f96acb21e2243bbc66c6a51f2d5
Joseph Steinbrenner <josstei@Josephs-MacBook-Air.local>
2025-08-18 02:12:39 -0400

    luxterm config update

--- lua/plugins/luxterm.lua
+++ lua/plugins/luxterm.lua
@@ -4,50 +4,25 @@ return dev.create_plugin_spec({
     "LuxVim/nvim-luxterm",
     config = function()
         require('luxterm').setup({
-            -- Manager window settings
+            -- Manager window dimensions (0.1 to 1.0)
             manager_width = 0.8,
             manager_height = 0.8,
-            border = "rounded",
-            left_pane_width = 0.3,
             
-            -- Preview settings
+            -- Enable live preview pane
             preview_enabled = true,
-            preview_max_lines = 1000,
-            preview_refresh_ms = 2000,
             
-            -- Session behavior
-            auto_close = false,
+            -- Focus new sessions when created via :LuxtermNew
             focus_on_create = false,
-            default_shell = nil,
-            session_name_template = "Terminal %d",
             
-            -- Performance options
-            cache_enabled = true,
-            lazy_render = true,
+            -- Auto-hide floating windows when cursor leaves
+            auto_hide = true,
             
-            -- Keymaps for manager window
+            -- Global keybinding configuration
             keymaps = {
-                new_session = "n",
-                close_manager = "<Esc>",
-                delete_session = "d",
-                rename_session = "r",
-                next_session = "<C-Right>",
-                prev_session = "<C-Left>",
-                select_session_1 = "1",
-                select_session_2 = "2",
-                select_session_3 = "3",
-                select_session_4 = "4",
-                select_session_5 = "5",
-                move_down = "j",
-                move_up = "k",
-            },
-            
-            -- Highlight groups
-            highlights = {
-                active_session = "PmenuSel",
-                inactive_session = "Pmenu",
-                border = "FloatBorder",
-                preview_border = "FloatBorder"
+                toggle_manager = "<C-/>",
+                next_session = "<C-]>",
+                prev_session = "<C-[>",
+                global_session_nav = false
             }
         })
         
