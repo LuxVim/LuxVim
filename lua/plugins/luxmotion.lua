dadc6f6099a0abcf66978e3c40fb618e7dad7b7d
josstei <48696594+josstei@users.noreply.github.com>
2026-01-29 23:46:42 -0500

    feat: update to leverage new luxmotion

--- lua/plugins/luxmotion.lua
+++ lua/plugins/luxmotion.lua
@@ -4,31 +4,6 @@ return dev.create_plugin_spec({
     "LuxVim/nvim-luxmotion",
     event = "VeryLazy",
     config = function()
-        require("luxmotion").setup({
-            cursor = {
-                duration = 250,
-                easing = "ease-out",
-                enabled = true,
-            },
-            scroll = {
-                duration = 400,
-                easing = "ease-out", 
-                enabled = true,
-            },
-            keymaps = {
-                cursor = true,
-                scroll = true,
-                experimental = true,
-            },
-            performance = {
-                enabled = false,  -- Can enable for testing
-                disable_syntax_during_scroll = true,
-                ignore_events = {'WinScrolled', 'CursorMoved', 'CursorMovedI'},
-                reduce_frame_rate = false,
-                frame_rate_threshold = 30,
-                auto_enable_on_large_files = true,
-                large_file_threshold = 5000,
-            },
-        })
+        require("luxmotion").setup({})
     end,
-}, { debug_name = "nvim-luxmotion" })
+})
