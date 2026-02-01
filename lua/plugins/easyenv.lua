cbd1a71032602a740420d6ed6599ad7e438282f2
josstei <48696594+josstei@users.noreply.github.com>
2026-02-01 14:13:02 -0500

    refactor: remove old configuration structure
    - Removed lua/config/lazy.lua (replaced by core.lib.bootstrap)
    - Removed lua/config/keymaps.lua (replaced by core.registry.keymaps)
    - Removed lua/config/autocmds.lua (replaced by core.registry.autocmds)
    - Removed lua/dev.lua (replaced by core.lib.debug)
    - Removed lua/utils.lua (no longer needed)
    - Removed old flat plugin files in lua/plugins/*.lua
    - Only category directories remain in lua/plugins/

--- lua/plugins/easyenv.lua
+++
@@ -1,6 +0,0 @@
-local dev = require('dev')
-
-return dev.create_plugin_spec({
-    "josstei/vim-easyenv",
-    cmd = { "EasyEnvCreate", "EasyEnvLoad", "EasyEnvEdit" },
-}, { debug_name = "vim-easyenv" })
