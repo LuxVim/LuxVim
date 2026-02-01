73758e0f2aa72d37db58e49edb45d1a8ac0e447b
josstei <48696594+josstei@users.noreply.github.com>
2026-02-01 09:13:57 -0500

    feat(core): add autocmd and filetype registries

---
+++ lua/core/registry/filetypes.lua
@@ -0,0 +1,44 @@
+return {
+  lua = {
+    tabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+  },
+
+  python = {
+    tabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    colorcolumn = "88",
+  },
+
+  markdown = {
+    wrap = true,
+    spell = true,
+    conceallevel = 2,
+  },
+
+  json = {
+    tabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+  },
+
+  yaml = {
+    tabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+  },
+
+  javascript = {
+    tabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+  },
+
+  typescript = {
+    tabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+  },
+}
