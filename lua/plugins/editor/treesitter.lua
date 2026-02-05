d4d3ab99027bfbf57d79c5d55a67e3f18cb5546f
josstei <48696594+josstei@users.noreply.github.com>
2026-02-05 15:07:01 -0500

    refactor: use paths module in treesitter config

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -6,8 +6,9 @@ return {
     priority = 900,
   },
   config = function()
+    local paths = require("core.lib.paths")
     local data_dir = vim.env.XDG_DATA_HOME or vim.fn.stdpath("data")
-    local parser_install_dir = data_dir .. "/data/site"
+    local parser_install_dir = paths.join(data_dir, "data", "site")
 
     require("nvim-treesitter.config").setup({
       install_dir = parser_install_dir,
