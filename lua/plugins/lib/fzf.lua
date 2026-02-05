7eb6afd2ff20f0183c57dd1acbce1ff37a26a9a7
josstei <48696594+josstei@users.noreply.github.com>
2026-02-05 15:07:05 -0500

    feat: add Windows build command for fzf

--- lua/plugins/lib/fzf.lua
+++ lua/plugins/lib/fzf.lua
@@ -2,6 +2,9 @@ return {
   source = "junegunn/fzf",
   build = {
     cmd = "./install --bin",
+    platforms = {
+      windows = "powershell -ExecutionPolicy Bypass -File .\\install.ps1",
+    },
     requires = { "git" },
   },
 }
