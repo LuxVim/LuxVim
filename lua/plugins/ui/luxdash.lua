015abbf1d9617a38e8762c3d406ac6c66ad08988
Your Name <you@example.com>
2026-09-14 01:37:46 -0400

    fix(dashboard): keep recent file targets stable

--- lua/plugins/ui/luxdash.lua
+++ lua/plugins/ui/luxdash.lua
@@ -2,7 +2,7 @@ return {
   source = "LuxVim/nvim-luxdash",
   event = "VimEnter",
   lazy = {
-    commit = "8e47b6a09544599c331b76e9c851a4033caaee67",
+    commit = "f34477220e415a9de65286fe3898dd02eae66d39",
   },
   opts = require("plugins.ui.config.luxdash"),
 }
