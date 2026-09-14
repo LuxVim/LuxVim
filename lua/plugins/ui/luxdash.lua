8ca15fb2fc101187ae3ff3c72c55455809e66133
Your Name <you@example.com>
2026-09-14 02:11:45 -0400

    fix(dashboard): render floating dashboard with context

--- lua/plugins/ui/luxdash.lua
+++ lua/plugins/ui/luxdash.lua
@@ -2,7 +2,7 @@ return {
   source = "LuxVim/nvim-luxdash",
   event = "VimEnter",
   lazy = {
-    commit = "f34477220e415a9de65286fe3898dd02eae66d39",
+    commit = "d2a31e57c523006c30ec859b5a2c3ae3a1b9c424",
   },
   opts = require("plugins.ui.config.luxdash"),
 }
