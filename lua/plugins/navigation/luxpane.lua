f7e879e82261fde6968e912f3df5221e53436421
Your Name <you@example.com>
2026-09-14 01:03:07 -0400

    fix(dashboard): open recent files in the current window

--- lua/plugins/navigation/luxpane.lua
+++ lua/plugins/navigation/luxpane.lua
@@ -1,8 +1,10 @@
 return {
   source = "LuxVim/vim-luxpane",
   event = "VeryLazy",
+  lazy = { commit = "2eb64eb7512ecb992ceacc6645671f3b28426747" },
   globals = {
     luxpane_protected_bt = { "quickfix", "help", "nofile", "terminal" },
     luxpane_protected_ft = { "NvimTree" },
+    luxpane_replaceable_ft = { "luxdash" },
   },
 }
