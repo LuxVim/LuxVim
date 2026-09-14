375cc03fcf05f9740336e7c296a859dbfd5b6230
Your Name <you@example.com>
2026-09-14 02:16:30 -0400

    fix(dashboard): support fzf.vim file finding

--- lua/plugins/ui/luxdash.lua
+++ lua/plugins/ui/luxdash.lua
@@ -2,7 +2,7 @@ return {
   source = "LuxVim/nvim-luxdash",
   event = "VimEnter",
   lazy = {
-    commit = "4b12c27f47cf0ed7c96c85a4659bb5c9d5531ad8",
+    commit = "c11f308c28f12b957309951be7c5825b716f3f2b",
   },
   opts = require("plugins.ui.config.luxdash"),
 }
