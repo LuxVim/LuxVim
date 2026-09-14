4e348979923cb3b69ed93ffe1d924e4bfe79444b
Your Name <you@example.com>
2026-09-14 02:12:46 -0400

    fix(dashboard): limit recent files to the project

--- lua/plugins/ui/luxdash.lua
+++ lua/plugins/ui/luxdash.lua
@@ -2,7 +2,7 @@ return {
   source = "LuxVim/nvim-luxdash",
   event = "VimEnter",
   lazy = {
-    commit = "d2a31e57c523006c30ec859b5a2c3ae3a1b9c424",
+    commit = "ba831398debdbdf3d086f61ef9584099afca976f",
   },
   opts = require("plugins.ui.config.luxdash"),
 }
