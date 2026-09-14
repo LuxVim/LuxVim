39f8b283920f9fb1d8fe6eed37b8fda7c1b4a654
Your Name <you@example.com>
2026-09-14 00:00:33 -0400

    fix(explorer): prevent dashboard shortcuts from overriding tree search

--- lua/plugins/ui/luxdash.lua
+++ lua/plugins/ui/luxdash.lua
@@ -1,5 +1,8 @@
 return {
   source = "LuxVim/nvim-luxdash",
   event = "VimEnter",
+  lazy = {
+    commit = "8e47b6a09544599c331b76e9c851a4033caaee67",
+  },
   opts = require("plugins.ui.config.luxdash"),
 }
